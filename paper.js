c94a98aab9f0e8992159338c772df7da35625c69
DevilGT4 <77671024+DevilGT4@users.noreply.github.com>
2021-06-09 17:04:48 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,21 @@
+class Paper 
+{
+    constructor(x,y,radius)
+	{
+		var options = {
+            isStatic: false,
+           ' restitution':0, 
+           'friction':0,
+           ' density':1.2  }
+
+
+        World.add(world,this.body)
+        this.image = loadImage("paper.png")
+   
+
+    }
+    display()
+		
+		
+
+	}
